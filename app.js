1f407df99e8d6b8142aadbf50a2566abc986982a
Saurav Bhattarai <sbhattarai@himalayantechies.com>
2021-06-08 18:17:48 +0545

    Folder structure set up for back end development

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+import 'dotenv/config';
+import express from 'express';
+const app = express();
+
+// Making the directory "public" static 
+app.use(express.static('public'))
+
+// Setting .ejs file to be viewed by default
+app.set("view engine", "ejs");
+
+
+// Landing Page Routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// // Undefined Route
+app.get("*", (req, res) => {
+    res.send("Page Not Found Bitch");
+});
+
+//Connecting to Database
+// mongoose.connect(process.env.DB_CONNECTION , {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}, () => {
+//     console.log("Connected to Database");
+// });
+
+// Listening to Server
+app.listen(3000, () => {
+    console.log("Growthademy Has Started at Server localhost://3000");
+});
