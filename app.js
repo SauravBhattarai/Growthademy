6eaa9250cb2240fe588780e2d7eefe65ca68f49d
Saurav Bhattarai <sbhattarai@himalayantechies.com>
2021-06-12 15:03:18 +0545

    Revert "Files deleted from main repository and kept in a folder"
    This reverts commit 79225ceabcd74c9b195b97c6acb1386df2032467.

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-import 'dotenv/config';
-import express from 'express';
+import "dotenv/config";
+import express from "express";
 const app = express();
 
-// Making the directory "public" static 
-app.use(express.static('public'))
+// Making the directory "public" static
+app.use(express.static("public"));
 
 // Setting .ejs file to be viewed by default
 app.set("view engine", "ejs");
 
-
-// Landing Page Routes
+// Landing Page Route
 app.get("/", (req, res) => {
-    res.render("index");
+  res.render("index");
+});
+
+// Articles Page Routes
+app.get("/articles", (req, res) => {
+  res.render("articles");
+});
+
+// Videos Page Routes
+app.get("/videos", (req, res) => {
+  res.render("videos");
+});
+
+// Audios Page Routes
+app.get("/audios", (req, res) => {
+  res.render("audios");
 });
 
 // // Undefined Route
 app.get("*", (req, res) => {
-    res.send("Page Not Found Bitch");
+  res.send("Page Not Found Bitch");
 });
 
 //Connecting to Database
@@ -26,5 +40,5 @@ app.get("*", (req, res) => {
 
 // Listening to Server
 app.listen(3000, () => {
-    console.log("Growthademy Has Started at Server localhost://3000");
+  console.log("Growthademy Has Started at Server localhost://3000");
 });
